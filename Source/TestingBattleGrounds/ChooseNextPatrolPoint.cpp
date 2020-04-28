dc0472f4421c7f319552e6a9f9cd6ed1c7653038
Fetuxion <Soulreaveroo@gmail.com>
2020-04-28 17:51:11 +0200

    Lecture(245. Reading Blackboard Data in C++)

--- Source/TestingBattleGrounds/ChooseNextPatrolPoint.cpp
+++ Source/TestingBattleGrounds/ChooseNextPatrolPoint.cpp
@@ -2,9 +2,15 @@
 
 
 #include "ChooseNextPatrolPoint.h"
+#include "behaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextPatrolPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Success"));
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	
+	
+	UE_LOG(LogTemp, Warning, TEXT("Index: %i"), Index)
+	
 	return EBTNodeResult::Type::Succeeded;
 }
