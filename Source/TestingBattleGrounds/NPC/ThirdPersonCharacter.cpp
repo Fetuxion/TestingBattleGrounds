c03d960d7bcdb455513e6fcdf51b09deafbd343f
Fetuxion <Soulreaveroo@gmail.com>
2020-10-14 22:16:00 +0200

    Lecture (266. Creating Animation State Machines)

---
+++ Source/TestingBattleGrounds/NPC/ThirdPersonCharacter.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonCharacter.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AThirdPersonCharacter::AThirdPersonCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a gun mesh component
+	FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+	FP_Gun->bCastDynamicShadow = false;
+	FP_Gun->CastShadow = false;
+	FP_Gun->SetupAttachment(GetMesh());
+}
+
+// Called when the game starts or when spawned
+void AThirdPersonCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
+	FP_Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+
+	GetMesh()->SetHiddenInGame(false, false);
+}
+
+// Called every frame
+void AThirdPersonCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AThirdPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+
---
+++ Source/TestingBattleGrounds/NPC/ThirdPersonCharacter.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ThirdPersonCharacter.generated.h"
+
+UCLASS()
+class TESTINGBATTLEGROUNDS_API AThirdPersonCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* FP_Gun;
+
+public:
+	// Sets default values for this character's properties
+	AThirdPersonCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
